f16102d53f5a58d7213ef9b1693f7bb7536ca86e
Bree2003 <breeale2003@gmail.com>
2024-05-26 20:14:36 -0400

    abiliting security and first configuration

--- src/main/java/com/bree2003/thymeleaf/controller/UserController.java
+++ src/main/java/com/bree2003/thymeleaf/controller/UserController.java
@@ -3,6 +3,7 @@ package com.bree2003.thymeleaf.controller;
 import com.bree2003.thymeleaf.entity.UserEntity;
 import com.bree2003.thymeleaf.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,9 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @GetMapping("/record")
     public String recordPage() {
         return "/users/register";
@@ -22,6 +26,7 @@ public class UserController {
 
     @PostMapping("/register")
     public String register(UserEntity user){
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userService.createUser(user);
         return "redirect:/login";
     }
---
+++ src/main/java/com/bree2003/thymeleaf/service/UserDetailsServiceImpl.java
@@ -0,0 +1,36 @@
+package com.bree2003.thymeleaf.service;
+
+import com.bree2003.thymeleaf.entity.UserEntity;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private IUserService userService;
+
+    @Autowired
+    private HttpSession session;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserEntity> optionalUser = userService.getUserByUsername(username);
+        if(optionalUser.isPresent()) {
+            session.setAttribute("user_session_id", optionalUser.get().getId());
+            UserEntity userEntity = optionalUser.get();
+            return User.builder()
+                    .username(userEntity.getUsername())
+                    .password(userEntity.getPassword())
+                    .roles()
+                    .build();
+        } else {
+            throw new UsernameNotFoundException("User not found!");
+        }
+    }
+}
